3d505e3bfeda0ee6c5205ea7e5a73ec6753ac0c0
Woon <a.vinsteen@gmail.com>
2024-10-12 23:34:21 +0300

    Updated version

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send(`Hello World! The super secret API key is: ${process.env.API_KEY}`);
+  res.send(`Hello World! The pipeline is still open... Also: ${process.env.API_KEY}`);
 });
 
 app.listen(port, () => {
