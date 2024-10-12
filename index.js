84e6f510c8f3d0a9f0e9a9dbb859b215ff52450c
Woon <a.vinsteen@gmail.com>
2024-10-12 22:58:00 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send(`Hello World!\nYour API key is: ${process.env.API_KEY}`);
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
